fafdb971927537a9cb87b7ba182f3385044386b2
Frank Lee <flee531@gmail.com>
2016-08-07 18:56:40 -0700

    Implement Web Worker use in client

--- client/app.js
+++ client/app.js
@@ -38,20 +38,26 @@ $(document).ready(function() {
   // - newJob
   // - updateWorkers
   // - updateResults
-  // - 
+  // - finalResult
 
   socket.on('updateWorkers', function(workers) {
     console.log('New workers list received:', workers);
   });
 
   socket.on('newJob', function(job) {
-    console.log('Working on new job:', job);
     
-    var result = findPrimes(job.data[0], job.data[1]);
-    job.result = result;
-    console.log('Job complete. Result is: ', result);
-    console.log('Sending result back to server');
-    socket.emit('userJobDone', job);
+    if (myWebWorker !== null) {
+      console.log('Web Worker assigned to the new job!');
+      myWebWorker.postMessage(job);
+
+    } else {
+      console.log('This browser does not support Web Workers. mapData will run in the main browser process.');
+      var result = findPrimes(job.data[0], job.data[1]);
+      job.result = result;
+      console.log('Job complete. Result is: ', result);
+      console.log('Sending result back to server');
+      socket.emit('userJobDone', job);
+    }
 
   });
 
@@ -68,6 +74,25 @@ $(document).ready(function() {
     console.log('Received final results!');
     $('#results').append('<li>Final result! Found primes: ' + final.length);
   });
+
+  /************************************************
+  // Web Worker
+  ************************************************/
+  var myWebWorker = null;
+
+  if (typeof(Worker) !== 'undefined') {
+    console.log('Initializing new Web Worker');
+    myWebWorker = new Worker('worker.js');
+  } else {
+    console.log('This browser does not support Web Workers. The main browser process will perform the calculations, which will likely cause noticeable delays.');
+  }
+
+  // myWebWorker will send back a Job object with the 
+  // data field populated. Send this object to the server
+  myWebWorker.onmessage = function(event) {
+    var job = event.data;
+    socket.emit('userJobDone', job);
+  }
 
   sendReady();
 });
