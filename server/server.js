26b69f3ff6624a666537a59e14dadc90ed2ec669
Frank Lee <flee531@gmail.com>
2016-08-08 16:32:23 -0700

    Add timer values to Project constructor

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ io.on('connect', (socket) => {
   // ProjectController will remove the Worker object associated with this
   // socket connection and reassign its work to another object.
   socket.on('disconnect', () => {
-    console.log('User disconnected');
+    console.log('User disconnected:', socket.id);
     pc.userDisconnect(socket.id);
   });
   
