0b71acab2844731c1413f00cea290ca144874109
Leah Loversky <ldloversky@gmail.com>
2016-08-09 16:34:37 -0700

    Add some content to home page

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -7,15 +7,15 @@ export default class App extends Component {
     return (
       <div className='app'>
 
-        <nav className="navbar navbar-default">
-          <div className="container-fluid">
-            <div className="navbar-header">
+        <nav className='navbar navbar-default'>
+          <div className='container-fluid'>
+            <div className='navbar-header'>
               <h1>Rally</h1>
             </div>
-            <ul className="nav navbar-nav">
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/menu">Algorithms</Link></li>
-              <li><Link to="/">Login</Link></li>  
+            <ul className='nav navbar-nav'>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/menu'>Algorithms</Link></li>
+              <li><Link to='/'>Login</Link></li>  
             </ul>
           </div>
         </nav>
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 class HomeView extends Component {
   render() {
     return (
-      <div>This is the home view</div>
+      <div>
+        <div>Yay crowdsourced distributed computing. This is more information about our app...</div>
+        <button><Link to='menu'>Begin</Link></button>
+      </div>
     )
   }
 }
