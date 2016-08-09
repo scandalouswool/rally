28a22a2c5750088d52aadf5c56a30e5e3ef6f61b
Clay Han <clayhan1@gmail.com>
2016-08-08 19:02:22 -0700

    Implemented small changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,2 +1,15 @@
-import World from './world';
-import Hello from './components/hello.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/app';
+import reducers from './reducers';
+
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
