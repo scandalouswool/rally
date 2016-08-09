df38600c879b88380b47ad44c6731f3b60d85e65
Leah Loversky <ldloversky@gmail.com>
2016-08-09 15:09:15 -0700

    Add files and skeletons for frontend components

---
+++ client/src/components/createAlgorithm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CreateAlgorithmView extends Component {
+  render() {
+    return (
+      <div>This is the create algorithm view</div>
+    )
+  }
+}
+
+export default CreateAlgorithmView;
---
+++ client/src/components/error.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ErrorView extends Component {
+  render() {
+    return (
+      <div>Error!!!</div>
+    )
+  }
+}
+
+export default ErrorView;
