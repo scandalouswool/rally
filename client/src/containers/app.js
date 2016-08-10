295787a2ded5d21b5d16670dd36af61b035995ab
Clay Han <clayhan1@gmail.com>
2016-08-09 20:54:11 -0700

    Completed the basic skeleton in redux, implemented sockets into app.js, added a navbar component

--- client/src/containers/app.js
+++ client/src/containers/app.js
@@ -10,6 +10,75 @@ export default class App extends Component {
 
   componentDidMount() {
     console.log('This is the socket state: ', this.socket);
+
+    const sendReady = () => {
+      // Hard coded for now - eventually generate from server
+      // and send to client for storage
+      var projectId = 'project0';  
+      this.socket.emit('userReady', projectId);
+    }
+
+    // Client must be able to handle the following events:
+    // - newJob
+    // - updateWorkers
+    // - updateResults
+    // - finalResult
+
+    this.socket.on('updateWorkers', (workers) => {
+      // console.log('New workers list received:', workers);
+      console.log('Updating workers in updateWorkers: ', workers);
+    });
+
+    this.socket.on('newJob', (job) => {
+      
+      // //if myWebWorker is not null, that means we were able to create it
+      // if (myWebWorker !== null) {
+      //   console.log('Web Worker assigned to the new job!');
+      //   //send the job item to the web worker, postMessage sends a message to the web worker
+      //   myWebWorker.postMessage(job);
+      // } else {
+      //   console.log('This browser does not support Web Workers. mapData will run in the main browser process.');
+
+      //   var mapDataFunc = eval('(' + job.mapData + ')');
+      //   job.result = mapDataFunc(job.data);
+
+      //   console.log('Job complete. Result is: ', job.result);
+      //   console.log('Sending result back to server');
+        
+      //   this.socket.emit('userJobDone', job);
+      // }
+      console.log('Updating job in newJob: ', job);
+
+      // const that = this;
+
+      setTimeout(() => {
+        console.log('Sending finished job');
+        job.result = [];
+        this.socket.emit('userJobDone', job);
+      }, 2000);
+    });
+
+    this.socket.on('updateResults', (results) => {
+      // console.log(results);
+      // $('#nQueensSolutions').empty();
+
+      // results.forEach( function(item) {
+      //   if (item !== null) {
+      //     $('#nQueensSolutions').append('<li>Worker found ' + item + ' solutions!');        
+      //   }
+
+      // });
+      console.log('Updating results in updateResults: ', results);
+    });
+
+    this.socket.on('finalResult', (final) => {
+      // console.log('Received final results!');
+      // $('#nQueensSolutions').append('<li>Final nQueens result after applying the mirror-image algorithm: ' + final);
+      console.log('Updating finalResult: ', final);
+    });
+
+    setTimeout(() => sendReady(), 2000);
+
   }
 
   render() {
