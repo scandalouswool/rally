1889c203e71acba425d2e78cb62d103d4760bad9
Frank Lee <flee531@gmail.com>
2016-08-12 15:54:52 -0700

    Revised default projects state in projects reducer

--- client/src/reducers/reducer_projects.js
+++ client/src/reducers/reducer_projects.js
@@ -3,7 +3,7 @@
 
 //This is hardcoded
 
-export default function (state = null, action) {
+export default function (state = {}, action) {
   switch(action.type) {
     case 'ALL_PROJECTS':
       console.log('Receiving the projects list:', action.payload);
