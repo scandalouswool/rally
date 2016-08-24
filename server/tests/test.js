9b7ec9dcf2ef36ef8499174785b33d3d6242c02e
Frank Lee <flee531@gmail.com>
2016-08-23 18:32:15 -0700

    Implement tests for Job constructor

--- server/tests/test.js
+++ server/tests/test.js
@@ -10,21 +10,6 @@ const io = require('socket.io-client');
 const Worker = require('../constructors/Worker.js');
 
 const testJob = new Job([1, 2, 3], 3, 'project01');
-const Job = require('../constructors/Job.js');
-const should = chai.should();
-const expect = chai.expect;
-
-const testJob = new Job([1, 2, 3], 3, 'project02');
-
-describe('Jobs', () => {
-  it('should be an object', () => {
-    testJob.should.be.a('object');
-  });
-  it('should should have a JobId', () => {
-    // testJob.to.include.keys('jobId');
-    should.exist(testJob.jobId);
-    expect(testJob.jobId).to.equal(3);
-  });
   it('should have a projectId', () => {
     should.exist(testJob.projectId);
     expect(testJob.projectId).to.equal('project02');
